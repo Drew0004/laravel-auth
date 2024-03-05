a16a15bd2f3e3eaaf1e652ddf197247d6d16c3f7
Drew0004 <drdrewgraphic@gmail.com>
2024-03-05 15:58:44 +0100

    Put Show crud in admin

---
+++ resources/views/admin/projects/show.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('page-title', 'Dashboard')
+
+@section('main-content')
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <h1 class="text-center text-success">
+                        Sei loggato!
+                    </h1>
+                    <br>
+                    Pagina Index
+                </div>
+            </div>
+            <div>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Slug</th>
+                            <th scope="col">Created at</th>
+                            <th scope="col">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    
+                        <tr>
+                            <th scope="row">{{ $project->id }}</th>
+                            <td>{{ $project->title }}</td>
+                            <td>{{ $project->slug }}</td>
+                            <td>{{ $project->created_at }}</td>
+                            <td>
+                                <a href="{{ route('admin.projects.show', ['project' => $project->slug]) }}" class="btn btn-xs btn-primary">
+                                    Show
+                                </a>
+                            </td>
+                        </tr>
+                       
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
