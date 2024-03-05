c5a168ae140ee578a982bb5b440780fba93875d2
Drew0004 <drdrewgraphic@gmail.com>
2024-03-05 15:31:01 +0100

    Put index crud in admin

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -14,6 +14,34 @@
                     Pagina Index
                 </div>
             </div>
+            <div>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Slug</th>
+                            <th scope="col">Created at</th>
+                            <th scope="col">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($projects as $project)
+                            <tr>
+                                <th scope="row">{{ $project->id }}</th>
+                                <td>{{ $project->title }}</td>
+                                <td>{{ $project->slug }}</td>
+                                <td>{{ $project->created_at }}</td>
+                                <td>
+                                    <a href="{{ route('admin.projects.show', ['project' => $project->slug]) }}" class="btn btn-xs btn-primary">
+                                        Show
+                                    </a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 @endsection
