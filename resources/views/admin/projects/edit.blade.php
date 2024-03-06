f8232731ce5df3e125ac5bf7517f1686fe543254
MattiaPaiano91 <paianomattia1@gmail.com>
2024-03-06 14:51:35 +0100

    Add frontend validation

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -4,22 +4,31 @@
 
 @section('main-content')
   <div>
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul class="mb-0">
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
         <form action="{{ route('admin.projects.update', ['project' => $project->slug]) }}" method="POST">
             @csrf
             @method('PUT')
             <div class="my-3">
                 <label for="title" class="form-label text-white">Titolo*</label>
-                <input value="{{ $project->title }}" type="text" class="form-control" id="title" name="title" placeholder="Inserisci il titolo..." maxlength="200" required>
+                <input value="{{ $project->title }}" type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" placeholder="Inserisci il titolo..." maxlength="200" required>
             </div>
             <div class="my-3">
                 <label for="description" class="form-label text-white">Descrizione*</label>
-                <textarea  class="form-control" id="description" name="description" rows="3" placeholder="Aggiungi una descrizione" maxlength="1024" required>
+                <textarea  class="form-control @error('description') is-invalid @enderror" id="description" name="description" rows="3" placeholder="Aggiungi una descrizione" maxlength="1024" required>
                 {{ $project->description }}
                 </textarea>
             </div>
             <div class="my-3">
                 <label for="client" class="form-label text-white">Cliente*</label>
-                <input value="{{ $project->client }}" type="text" class="form-control" id="client" name="client" placeholder="Inserisci il cliente..." maxlength="46" required>
+                <input value="{{ $project->client }}" type="text" class="form-control @error('client') is-invalid @enderror" id="client" name="client" placeholder="Inserisci il cliente..." maxlength="46" required>
             </div>
             <button class="btn btn-primary" type="submit">
                 Modifica +
